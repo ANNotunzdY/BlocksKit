b096d46eeb607f48b9fdeb87714cd1826c926e38
Takayoshi Sato <webmaster@smilemac.com>
2017-03-17 15:11:55 +0900

    remove UIImagePickerController from Header file

--- BlocksKit/BlocksKit+UIKit.h
+++ BlocksKit/BlocksKit+UIKit.h
@@ -33,7 +33,6 @@
 #import <BlocksKit/UIControl+BlocksKit.h>
 #import <BlocksKit/UIGestureRecognizer+BlocksKit.h>
 #import <BlocksKit/UIImage+BlocksKit.h>
-#import <BlocksKit/UIImagePickerController+BlocksKit.h>
 #import <BlocksKit/UIPopoverController+BlocksKit.h>
 #import <BlocksKit/UITextField+BlocksKit.h>
 #import <BlocksKit/UIView+BlocksKit.h>
